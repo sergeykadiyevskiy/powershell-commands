d8ec08c7e5baea0785bbf5572185aa6e048206b1
Sergey Kadiyevskiy <51814805+sergeykadiyevskiy@users.noreply.github.com>
2022-08-30 15:04:37 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// Download "FortiClientTools_5.4.0.0780.zip" from support.
+// Use FortiSSLVPNclient.exe.
+// FortiSSLVPNclient connect -h xxx.xxx.xxx.xxx:portnumber -u username:password
+// FortiSSLVPNclient disconnect
+// There are other options, using a pre saved configuration.
+
+const { exec } = require('child_process');
+exec('start Photoshop.exe ', {'shell':'powershell.exe'}, (error, stdout, stderr)=> {
+    // do whatever with stdout
+})
